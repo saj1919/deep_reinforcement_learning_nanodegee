cd16f493e0d432a0e1f9a9022515ca8ca697c76c
Swapnil Jadhav <saj1919@hotmail.com>
2018-11-11 13:43:26 +0530

    Update model.py

--- p2_continuous-control/model.py
+++ p2_continuous-control/model.py
@@ -10,18 +10,8 @@ def hidden_init(layer):
     return (-lim, lim)
 
 class Actor(nn.Module):
-    """Actor (Policy) Model."""
 
     def __init__(self, state_size, action_size, seed=0, fc1_units=64, fc2_units=128):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
-        """
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
@@ -37,7 +27,6 @@ class Actor(nn.Module):
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
-        """Build an actor (policy) network that maps states -> actions."""
         if state.dim() == 1:
             state = torch.unsqueeze(state,0)
         x = F.relu(self.fc1(state))
@@ -47,18 +36,7 @@ class Actor(nn.Module):
 
 
 class Critic(nn.Module):
-    """Critic (Value) Model."""
-
     def __init__(self, state_size, action_size, seed=0, fcs1_units=64, fc2_units=128):
-        """Initialize parameters and build model.
-        Params
-        ======
-            state_size (int): Dimension of each state
-            action_size (int): Dimension of each action
-            seed (int): Random seed
-            fcs1_units (int): Number of nodes in the first hidden layer
-            fc2_units (int): Number of nodes in the second hidden layer
-        """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fcs1 = nn.Linear(state_size, fcs1_units)
@@ -74,7 +52,6 @@ class Critic(nn.Module):
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
-        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         if state.dim() == 1:
             state = torch.unsqueeze(state,0)
         xs = F.relu(self.fcs1(state))
